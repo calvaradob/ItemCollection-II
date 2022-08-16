e76691e2b9649009218ddfed18c46bf9b4076fbc
desafio <calvaradob@falabella.cl>
2022-08-16 17:06:24 -0300

    limp

--- Ejemplo e-commerce/src/constants/products.js
+++
@@ -1,6 +0,0 @@
-export const Sistemas = '/images/sistemasop.png';
-export const Windows = '/images/windows.jpg';
-export const TangoGestion = '/images/tangogest.png';
-export const PCGamer = '/images/pcgamer.jpg';
-export const SoftwareAMedida = '/images/software_a_medida.jpeg';
-export const Ipad = '/images/ipad.jpg';
